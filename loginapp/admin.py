365deb5eca542aaa365da746ca5de294afb7733b
NARASIMHA SAI ADDEPALLI <38582295+Narasimha-sai@users.noreply.github.com>
2020-10-12 15:56:50 +0530

    Add files via upload

---
+++ loginapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from loginapp.models import Migration
+from loginapp.models import BmiData
+# Register your models here.
+
+admin.site.register(Migration)
+admin.site.register(BmiData)
