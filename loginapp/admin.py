e1e6d8b33db61614d06d7da3c71a464f1b278355
NSai99 <80258427+NSai99@users.noreply.github.com>
2021-04-29 12:34:05 +0530

    coment admin.py

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from loginapp.models import Migration
 from loginapp.models import BmiData
 # Register your models here.
-
+# admin.py databases are registered here
 admin.site.register(Migration)
 admin.site.register(BmiData)
